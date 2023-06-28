d9f5e2736c18a4ef6fef79fe8bc30a3da9a020e8
Hooleymass <hockleymasese3@gmail.com>
2023-06-28 16:59:59 -0700

    exec install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,18 @@ class { 'python3::pip3'
   ensure   => 'latest',
 }
 
+package { 'python3':
+  ensure => '3.8.10',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Class['python3::pip3'],
+}
+
+package { 'Flask':
+  ensure   => '2.1.1',
+  provider => 'pip3', 
   require  => Class['python3::pip3'],
 }
