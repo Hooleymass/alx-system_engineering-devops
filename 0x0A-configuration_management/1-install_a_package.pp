af7a64e1a926916ea68097bbf3af1cdfdd0cced9
Hooleymass <hockleymasese3@gmail.com>
2023-06-28 16:45:46 -0700

    exec install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Installing a flask using pip3
-
+class { 'python3::pip3'
+  ensure   => 'latest',
+  provider => 'pip3',
+}
 exec { 'pip3 install flask':
-  command => '/usr/bin/pip3 install flask',
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
