ebb9d31b058b0132c46087c98a67da50b4cdd0d9
Hooleymass <hockleymasese3@gmail.com>
2023-06-28 18:00:29 -0700

    config server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-# server configuration
+# SSH client configuration
 #
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/sshd_config',
-  line => 'PasswordAuthentication no',
+file_line { 'Configure identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
 }
 
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
+file_line { 'Disable password authentication':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
 }
 
