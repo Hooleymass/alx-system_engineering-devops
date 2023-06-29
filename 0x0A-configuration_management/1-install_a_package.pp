4f79f04f63b9c7b6ca52cbc282a7cc5d5e64ae17
Hooleymass <hockleymasese3@gmail.com>
2023-06-28 17:13:58 -0700

    package install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Installing a flask using pip3
+# Puppet manifest to install Flask package
+
+package { 'python3-pip':
+  ensure => installed,
+}
 
 package { 'Flask':
-  ensure   => '2.1.1',
+  ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
